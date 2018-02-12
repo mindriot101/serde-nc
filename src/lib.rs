830889f49dcada2a75011fb9d30672af04e11404
Simon Walker <s.r.walker101@googlemail.com>
2018-02-12 21:20:19 +0000

    set up tests

--- src/lib.rs
+++ src/lib.rs
@@ -5,3 +5,13 @@ mod ser;
 
 pub use error::{Error, Result};
 pub use ser::{to_string, Serializer};
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_it_works() {
+        assert_eq!(1, 1);
+    }
+}
