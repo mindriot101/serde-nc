3e947fe76dea035ae887703996aacf57fc82e994
Simon Walker <simon.walker@the-mtc.org>
2018-02-12 09:28:08 +0000

    Implement supplimentary traits

--- src/ser.rs
+++ src/ser.rs
@@ -27,3 +27,131 @@ impl<'a> ser::Serializer for &'a mut Serializer {
     type SerializeStruct = Self;
     type SerializeStructVariant = Self;
 }
+
+impl<'a> ser::SerializeStructVariant for &'a mut Serializer {
+    type Ok = ();
+    type Error = Error;
+
+    fn serialize_field<T>(&mut self, key: &'static str, value: &T) -> Result<()>
+        where T: ?Sized + Serialize
+        {
+            unimplemented!();
+        }
+
+    fn end(self) -> Result<()> {
+        unimplemented!();
+    }
+}
+
+impl<'a> ser::SerializeMap for &'a mut Serializer {
+    type Ok = ();
+    type Error = Error;
+
+    fn serialize_key<T>(&mut self, key: &T) -> Result<()>
+        where T: ?Sized + Serialize {
+        unimplemented!();
+    }
+
+    fn serialize_value<T>(&mut self, value: &T) -> Result<()>
+        where T: ?Sized + Serialize {
+    }
+
+    fn end(self) -> Result<()> {
+        unimplemented!();
+    }
+}
+
+impl<'a> ser::SerializeStruct for &'a mut Serializer {
+    type Ok = ();
+    type Error = Error;
+
+    fn serialize_field<T: ?Sized>(
+        &mut self, 
+        key: &'static str, 
+        value: &T
+    ) -> Result<()>
+    where
+        T: Serialize
+        {
+            unimplemented!();
+        }
+    fn end(self) -> Result<()> {
+        unimplemented!();
+    }
+}
+
+impl<'a> ser::SerializeTupleVariant for &'a mut Serializer {
+    type Ok = ();
+    type Error = Error;
+
+    fn serialize_field<T: ?Sized>(
+        &mut self, 
+        key: &'static str, 
+        value: &T
+    ) -> Result<()>
+    where
+        T: Serialize
+        {
+            unimplemented!();
+        }
+    fn end(self) -> Result<()> {
+        unimplemented!();
+    }
+}
+
+impl<'a> ser::SerializeTupleStruct for &'a mut Serializer {
+    type Ok = ();
+    type Error = Error;
+
+    fn serialize_field<T: ?Sized>(
+        &mut self, 
+        key: &'static str, 
+        value: &T
+    ) -> Result<()>
+    where
+        T: Serialize
+        {
+            unimplemented!();
+        }
+    fn end(self) -> Result<()> {
+        unimplemented!();
+    }
+}
+
+impl<'a> ser::SerializeTuple for &'a mut Serializer {
+    type Ok = ();
+    type Error = Error;
+
+    fn serialize_element<T: ?Sized>(
+        &mut self, 
+        key: &'static str, 
+        value: &T
+    ) -> Result<()>
+    where
+        T: Serialize
+        {
+            unimplemented!();
+        }
+    fn end(self) -> Result<()> {
+        unimplemented!();
+    }
+}
+
+impl<'a> ser::SerializeSeq for &'a mut Serializer {
+    type Ok = ();
+    type Error = Error;
+
+    fn serialize_element<T: ?Sized>(
+        &mut self, 
+        key: &'static str, 
+        value: &T
+    ) -> Result<()>
+    where
+        T: Serialize
+        {
+            unimplemented!();
+        }
+    fn end(self) -> Result<()> {
+        unimplemented!();
+    }
+}
